2989fb01dace5219bfaf8bcea74acd2cbff22909
Tim Weber <scy@scy.name>
2013-05-17 19:58:13 -0400

    Start working on template and JS

---
+++ om-signup.js
@@ -0,0 +1,40 @@
+(function () {
+	'use strict';
+	var catalogue = {
+		'UBER-EB': {
+			name: 'mit Übernachtung (Early Bird)',
+			price: 70
+		},
+		'UBER': {
+			name: 'mit Übernachtung',
+			price: 90
+		},
+		'UBER-FT': {
+			name: 'mit Übernachtung (meine erste openmind)',
+			price: 90
+		},
+		'UBER-ST': {
+			name: 'mit Übernachtung (Supporter-Ticket)',
+			price: 100,
+			priceEditable: true
+		},
+		'KONF': {
+			name: 'ohne Übernachtung',
+			price: 33
+		},
+		'KONF-FT': {
+			name: 'ohne Übernachtung (meine erste openmind)',
+			price: 33
+		},
+		'KONF-ST': {
+			name: 'ohne Übernachtung (Supporter-Ticket)',
+			price: 40,
+			priceEditable: true
+		},
+		'SHIRT': {
+			name: 'T-Shirt',
+			type: 'shirt',
+			price: 25
+		}
+	};
+})();
